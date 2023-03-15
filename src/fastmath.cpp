0ffe502d3e46eab8de8f16f523f4f4c8914297fa
Nick Smallbone <nick@smallbone.se>
2023-03-15 22:36:25 +0100

    And remember to use fixed_fastpowf in fastmath.cpp...

--- src/fastmath.cpp
+++ src/fastmath.cpp
@@ -56,7 +56,7 @@ static inline float fast_fmodf(float x, float y) { return fmodf(x, y); }
 static inline float fast_logf(float x) { return fastlogf(x); }
 static inline float fast_log2f(float x) { return fastlog2f(x); }
 static inline float fast_log10f(float x) { return (M_LOG10E * M_LN2) * fastlog2f(x); }
-static inline float fast_powf(float x, float y) { return fastpowf(x, y); }
+static inline float fast_powf(float x, float y) { return fixed_fastpowf(x, y); }
 static inline float fast_remainderf(float x, float y) { return remainderf(x, y); }
 static inline float fast_rintf(float x) { return rintf(x); }
 static inline float fast_roundf(float x) { return roundf(x); }
