0bab8a254573ec3c1379ed6bf93162aee49d6c12
Nick Smallbone <nick@smallbone.se>
2023-03-16 11:26:32 +0100

    Also replace fastexpf.

--- src/fastmath.cpp
+++ src/fastmath.cpp
@@ -31,15 +31,6 @@ float fixed_fastpow2f(float p) {
   return v.f;
 }
 
-/** "Fast" x to the power of p approximation
- * @note Adapted from Paul Mineiro's FastFloat
- * @note Warning: Seems to have divergent segments with discontinuities for some base/exponent combinations
- */
-static inline __attribute__((optimize("Ofast"), always_inline))
-float fixed_fastpowf(float x, float p) {
-  return fixed_fastpow2f(p * fastlog2f(x));
-}
-
 static inline float fast_fabsf(float x) { return si_fabsf(x); }
 static inline float fast_acosf(float x) { return acosf(x); }
 static inline float fast_asinf(float x) { return asinf(x); }
@@ -47,16 +38,16 @@ static inline float fast_atanf(float x) { return atanf(x); }
 static inline float fast_atan2f(float x, float y) { return atan2f(x, y); }
 static inline float fast_ceilf(float x) { return ceilf(x); }
 static inline float fast_cosf(float x) { return fastcosfullf(x); }
-static inline float fast_expf(float x) { return fastexpf(x); }
-static inline float fast_exp2f(float x) { return fastexpf(M_LN2 * x); };
-static inline float fast_exp10f(float x) { return fastexpf(M_LN10 * x); };
+static inline float fast_expf(float x) { return fixed_fastpow2f(M_LOG2E * x); }
+static inline float fast_exp2f(float x) { return fixed_fastpow2f(x); };
+static inline float fast_exp10f(float x) { return fixed_fastpow2f((M_LOG2E * M_LN10) * x); };
 // si_floorf only works on uint32_t - same applies to ceil, round
 static inline float fast_floorf(float x) { return floorf(x); }
 static inline float fast_fmodf(float x, float y) { return fmodf(x, y); }
 static inline float fast_logf(float x) { return fastlogf(x); }
 static inline float fast_log2f(float x) { return fastlog2f(x); }
 static inline float fast_log10f(float x) { return (M_LOG10E * M_LN2) * fastlog2f(x); }
-static inline float fast_powf(float x, float y) { return fixed_fastpowf(x, y); }
+static inline float fast_powf(float x, float y) { return fixed_fastpow2f(fastlog2f(x) * y); }
 static inline float fast_remainderf(float x, float y) { return remainderf(x, y); }
 static inline float fast_rintf(float x) { return rintf(x); }
 static inline float fast_roundf(float x) { return roundf(x); }
